0f9eecddce23087a8eda3e5efb3ca8c40973a8f7
pichichi21 <pichichi21@yahoo.com>
2017-06-26 10:37:34 -0700

    assignment

---
+++ WebFundamentals/Javascript/IdoDeclare.js
@@ -0,0 +1,25 @@
+var myFirstNum = 5;
+var mySecondNum = 7804343;
+var myThirdNum = -9;
+var myFourthNum = 9.89;
+
+var myFirstString = "hey";
+var mySecondString = "\"in quotes\"";
+var myThirdString = "  "; // empty string
+
+var notTrue = false;
+var veryTrue = true;
+
+var anUndefinedVariable; //undefined
+
+console.log("this is my first number: " + myFirstNum);
+console.log("this is my second number: " + mySecondNum);
+console.log("this is my third number: " + myThirdNum);
+console.log("this is my fourth number: " + myFourthNum);
+console.log("this is my first string: " + myFirstString);
+console.log("this is my second string: " + mySecondString);
+console.log("this is my third string, which happens to be only whitespace: " + myThirdString);
+console.log("this is a false boolean: " + notTrue);
+console.log("this is a true boolean: " + veryTrue);
+console.log("this is an undefined variable: " + anUndefinedVariable);
+
